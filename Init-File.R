6d8055d44ed6b287b0dd4579768a8f7c80089844
ernestrepo <ernestojhn5@gmail.com>
2022-12-29 19:45:07 +0000

    Prueba de primer commit en githhub

---
+++ Init-File.R
@@ -0,0 +1,8 @@
+#Instalar librerias para curso de ciencia de datos
+install.packages("tidyverse")
+install.packages("dslabs")
+
+#Cargar las librerias instaladas
+library(tidyverse)
+library(dslabs)
+
